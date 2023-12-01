d684f8e3edec05e0e2cb64b5878279b6b99f98b0
SaharMFR <sweetysahora@gmail.com>
2023-11-30 16:43:25 -0800

    update in task-15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -14,7 +14,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (1);
 
 	if (tree->right && tree->left)
-		return (binary_tree_is_full(tree->right) && bianry_tree_is_full(tree->left));
+		return (binary_tree_is_full(tree->right) &&
+			bianry_tree_is_full(tree->left));
 
 	return (0);
 }
